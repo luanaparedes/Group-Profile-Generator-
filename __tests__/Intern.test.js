3d7f0e22c4a095419d655551b861f1db191f4d38
luanaparedes <lua123na@gmail.com>
2023-01-03 12:30:32 -0500

    started tests for intern employee and engineer

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,41 @@
+const Intern = require('../lib/Intern');
+
+describe ('Intern Class', () => {
+    it('gets constructor values for intern class', () => {
+        const intern = new Intern('Kim', '22', 'kimwolt@gmail.com', 'RVCC');
+        expect(intern.name).toBe('Kim');
+        expect(intern.id).toBe('22');
+        expect(intern.email).toBe('kimwolt@gmail.com');
+        expect(intern.school).toBe('RVCC');
+    });
+    
+    describe ('getName', () => {
+        it('returns intern name', () => {
+            expect(intern.getName()).toBe('Kim');
+        });
+    });
+
+    describe ('getId', () => {
+        it('returns intern ID number', () => {
+            expect(intern.getId()).toBe('22');
+        });
+    });
+
+    describe ('getEmail', () => {
+        it('returns intern email address', () => {
+            expect(intern.getEmail()).toBe('kimwolt@gmail.com');
+        });
+    });
+    
+    describe ('getSchool', () => {
+        it('returns intern school name', () => {
+            expect(intern.getSchool()).toBe('RVCC');
+        });
+    });
+    
+    describe ('getRole', () => {
+        it('returns eployee role, in this case intern', () => {
+            expect(intern.getRole()).toBe('Intern');
+        });
+    });
+});
